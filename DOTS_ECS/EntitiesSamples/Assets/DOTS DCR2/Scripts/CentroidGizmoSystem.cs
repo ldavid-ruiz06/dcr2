0608da69c726773c4286c3d5af527ff0973f2ca1
Deibi <manco.dev1407@gmail.com>
2026-06-18 12:02:01 -0400

    gizmos siguen bancos diff confirmado

--- DOTS_ECS/EntitiesSamples/Assets/DOTS DCR2/Scripts/CentroidGizmoSystem.cs
+++ DOTS_ECS/EntitiesSamples/Assets/DOTS DCR2/Scripts/CentroidGizmoSystem.cs
@@ -102,10 +102,10 @@ namespace DCR2
                     i++;
                 }
 
-                for(i = 0; i < centroidCount; i++)
-                {
-                    Debug.Log(FixedString.Format("{0}", uniqueFishID[i]));
-                }
+                // for(i = 0; i < centroidCount; i++)
+                // {
+                //     Debug.Log(FixedString.Format("{0}", uniqueFishID[i]));
+                // }
 
                 
 
@@ -120,9 +120,11 @@ namespace DCR2
                 {
                     int x = uniqueFishID[g];
                     //Debug.Log(FixedString.Format("uniqueFish ID: {0}", x));
+                    var centroidVal = state.EntityManager.GetComponentData<DynamicSchool>(entityArray[x]).centroid;
                     var localToWorld = new LocalToWorld
                             {
-                                Value = float4x4.TRS(localToWorldLookup[entityArray[x]].Position, quaternion.LookRotationSafe(new float3(0f,0f,0f), math.up()), new float3(10.0f, 10.0f, 10.0f))
+                                // Value = float4x4.TRS(localToWorldLookup[entityArray[x]].Position, quaternion.LookRotationSafe(new float3(0f,0f,0f), math.up()), new float3(10.0f, 10.0f, 10.0f))
+                                Value = float4x4.TRS(centroidVal, quaternion.LookRotationSafe(new float3(0f,0f,0f), math.up()), new float3(10.0f, 10.0f, 10.0f))
                             };
                     localToWorldLookup[centroidEntityArray[g]] = localToWorld;
                 }
